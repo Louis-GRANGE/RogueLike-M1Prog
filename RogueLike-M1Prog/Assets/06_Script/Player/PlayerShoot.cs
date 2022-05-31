bd358f754374b07b0b26a62c3895420b3bef6f10
Jules Rouault <trustprinciples@gmail.com>
2022-05-31 10:44:11 +0200

    [ADD] PickItem & Pickable Weapons

--- RogueLike-M1Prog/Assets/06_Script/Player/PlayerShoot.cs
+++ RogueLike-M1Prog/Assets/06_Script/Player/PlayerShoot.cs
@@ -7,8 +7,12 @@ public class PlayerShoot : MonoBehaviour
     [Header("External References")]
     Camera _mainCamera;
 
-    [Header("Components")]
-    public Transform canon;
+    [Header("External Components")]
+    public Transform weaponHandler;
+    Transform _canon;
+    Weapon _weapon;
+
+    [Header("Internal Components")]
     ParticleSystem _cannonFire;
     Animator _animator;
 
@@ -24,38 +28,21 @@ public class PlayerShoot : MonoBehaviour
 
     private void Awake()
     {
-        _cannonFire = canon.GetChild(0).GetComponent<ParticleSystem>();
         _animator = transform.GetChild(0).GetComponent<Animator>();
     }
 
     private void Start()
     {
         _mainCamera = Camera.main;
-
-        _laserPool = new GameObject().transform;
-        _laserPool.parent = transform;
-        _laserPool.gameObject.SetActive(false);
-        for (int i = 0; i < 50; i++)
-        {
-            GameObject newLaser = Instantiate(laserFX, transform.position, transform.rotation, _laserPool);
-            newLaser.GetComponent<LaserEffect>().Initiate(_laserPool);
-        }
-
-        _hitPool = new GameObject().transform;
-        _hitPool.parent = transform;
-        _hitPool.gameObject.SetActive(false);
-        for (int i = 0; i < 50; i++)
-        {
-            GameObject newHit = Instantiate(hitFX, transform.position, transform.rotation, _hitPool);
-            newHit.GetComponent<HitEffect>().Initiate(_hitPool);
-        }
-
-        _fireRateTime = fireRateLatency;
+        //EquipWeapon(Resources.Load<WeaponData>("WeaponData/Automatic"));
     }
 
     private void Update()
     {
-        Shoot();
+        if(_weapon)
+            Shoot();
+
+        Interact();
     }
 
     public void Shoot()
@@ -75,16 +62,68 @@ public class PlayerShoot : MonoBehaviour
                 pointDirection = hit.point;
 
             pointDirection += new Vector3(Random.Range(-0.3f, 0.3f), 0, Random.Range(-0.3f, 0.3f));
-            Vector3 shootDirection = (pointDirection - canon.transform.position).normalized;
-            if (Physics.Raycast(canon.transform.position, shootDirection, out hit, 1000))
+            Vector3 shootDirection = (pointDirection - _canon.transform.position).normalized;
+            if (Physics.Raycast(_canon.transform.position, shootDirection, out hit, 1000))
             {
                 
                 _laserPool.GetChild(0).GetComponent<LaserEffect>().DrawLine(_cannonFire.transform.position, hit.point);
                 _hitPool.GetChild(0).GetComponent<HitEffect>().DrawParticle(hit.point);
             }
-
-            _animator.SetTrigger("Fire");
+            
             _cannonFire.Play();
+        }
+    }
+
+    public void EquipWeapon(WeaponData _newWeapon)
+    {
+        _weapon = Instantiate(_newWeapon.weaponPrefab, weaponHandler).GetComponent<Weapon>();
+        _canon = _weapon.canon;
+        _cannonFire = _canon.GetChild(0).GetComponent<ParticleSystem>();
+
+        fireRateLatency = _newWeapon.fireLatency;
+
+        if(_laserPool)
+            Destroy(_laserPool.gameObject);
+
+        _laserPool = new GameObject().transform;
+        _laserPool.parent = transform;
+        _laserPool.gameObject.SetActive(false);
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject newLaser = Instantiate(laserFX, transform.position, transform.rotation, _laserPool);
+            newLaser.GetComponent<LaserEffect>().Initiate(_laserPool);
+        }
+
+        if(_hitPool)
+            Destroy(_hitPool.gameObject);
+
+        _hitPool = new GameObject().transform;
+        _hitPool.parent = transform;
+        _hitPool.gameObject.SetActive(false);
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject newHit = Instantiate(hitFX, transform.position, transform.rotation, _hitPool);
+            newHit.GetComponent<HitEffect>().Initiate(_hitPool);
+        }
+
+        _fireRateTime = fireRateLatency;
+
+        _animator.SetTrigger("Equip");
+    }
+
+    public void Interact()
+    {
+        Vector3 pointDirection = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(pointDirection, _mainCamera.transform.forward, out hit, 1000))
+        {
+            if (hit.collider.GetComponent<WeaponItem>() && Vector3.Distance(transform.position, hit.point) < 3f && Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(_weapon);
+                EquipWeapon(hit.collider.GetComponent<WeaponItem>().weaponData);
+                Destroy(hit.collider.gameObject);
+            }
         }
     }
 }
---
+++ RogueLike-M1Prog/Assets/06_Script/Weapon/WeaponData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "ScriptableObjects/WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    public GameObject weaponPrefab;
+    public int damages;
+    public float fireLatency;
+}
