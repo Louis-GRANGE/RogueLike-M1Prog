c519f2a343fd8c60917acf3f1e34a9a93265637b
Jules Rouault <trustprinciples@gmail.com>
2022-06-21 13:17:19 +0200

    [ADD] Enemy vision

--- RogueLike-M1Prog/Assets/06_Script/Enemy/States/ShootPlayer.cs
+++ RogueLike-M1Prog/Assets/06_Script/Enemy/States/ShootPlayer.cs
@@ -6,6 +6,11 @@ using UnityEngine.AI;
 [CreateAssetMenu(menuName = "States/ShootPlayer")]
 public class ShootPlayer : AState
 {
+    [Header("Timer")]
+    float latency = 1;
+    float time;
+    bool canShoot;
+
     public override void StartState(AMainData mainData)
     {
         base.StartState(mainData);
@@ -13,7 +18,29 @@ public class ShootPlayer : AState
 
     public override void ExecuteState()
     {
-        ShootOnPlayer();
+        if (Player.Instance && time < latency)
+        {
+            time += Time.deltaTime;
+            if (time >= latency)
+            {
+                time = 0;
+                latency = Random.Range(0.2f, 0.6f);
+
+                RaycastHit hit;
+                Vector3 dir = (Player.Instance.transform.position - _mainData.transform.position).normalized;
+                if (Physics.Raycast(_mainData.transform.position, dir, out hit, 50))
+                {
+                    Debug.Log(hit.collider);
+                    if (hit.collider.CompareTag(Constants.TagPlayer))
+                        canShoot = true;
+                    else
+                        canShoot = false;
+                }
+            }
+        }
+
+        if (canShoot)
+            ShootOnPlayer();
     }
 
     public override void EndState()
